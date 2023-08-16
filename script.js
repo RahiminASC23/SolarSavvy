a3c6cdf3c1c77bf299b1c2e005de5e176c218438
RahiminASC23 <ramohammed@stu.allstarcode.org>
2023-08-15 23:09:42 -0400

    not done yet, still experimenting

--- script.js
+++ script.js
@@ -1,99 +1,52 @@
-//SCROLL ANIMATION
-const text = document.getElementsByClassName("r1");
-const image = document.getElementsByClassName('i1'); 
+// SCROLL ANIMATION
+const text = document.querySelector(".r1");
+const image = document.querySelector('.i1');
 
 window.addEventListener("scroll", appearOnScroll);
 
 function appearOnScroll() {
   let textPosition = text.getBoundingClientRect().top;
-  let windowHeight = window.innerHeight/1.5;
-  if(windowHeight > textPosition) {
+  let windowHeight = window.innerHeight / 1.5;
+  if (windowHeight > textPosition) {
     text.classList.remove("push_left");
     image.classList.remove("push_right");
-  }
-  else {
+  } else {
     text.classList.add("push_left");
     image.classList.add("push_right");
   }
 }
 
+// API CODE
+document.getElementById("zipcode-form").addEventListener("submit", function (e) {
+    e.preventDefault();
 
-//API CODEok
+    const zipcode = document.getElementById("zipcode-input").value;
+    const apiKey = "fsq3y0lGHgGJwsHZEldogtami5ozh65BAM4qhB4bObZViIU="; 
 
-// This example adds a search box to a map, using the Google Place Autocomplete
-// feature. People can enter geographical searches. The search box will return a
-// pick list containing a mix of places and predicted search terms.
-// This example requires the Places library. Include the libraries=places
-// parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-/*  */function initAutocomplete() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -33.8688, lng: 151.2195 },
-    zoom: 13,
-    mapTypeId: "roadmap",
-  });
-  // Create the search box and link it to the UI element.
-  const input = document.getElementById("pac-input");
-  const searchBox = new google.maps.places.SearchBox(input);
-
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener("bounds_changed", () => {
-    searchBox.setBounds(map.getBounds());
+    fetch(`https://api.foursquare.com/v3/venues/search?near=${zipcode}&radius=80500&query=solar+panel+providers&client_id=${apiKey}&v=20230815`)
+      .then(response => response.json())
+      .then(data => {
+        const venues = data.response.venues;
+        displayVenues(venues);
+      })
+      .catch(error => console.error(error));
   });
 
-  let markers = [];
+  function displayVenues(venues) {
+    const resultsContainer = document.getElementById("results");
+    resultsContainer.innerHTML = ""; // Clear previous results
 
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener("places_changed", () => {
-    const places = searchBox.getPlaces();
+    venues.forEach(venue => {
+      const name = venue.name;
+      const address = venue.location.address || "Address not available";
+      const phone = venue.contact.phone || "Phone number not available";
 
-    if (places.length == 0) {
-      return;
-    }
+      const venueElement = document.createElement("div");
+      venueElement.className = "venue";
+      venueElement.innerHTML = `<h3>${name}</h3><p>${address}</p><p>${phone}</p>`;
 
-    // Clear out the old markers.
-    markers.forEach((marker) => {
-      marker.setMap(null);
+      resultsContainer.appendChild(venueElement);
     });
-    markers = [];
+  }
 
-    // For each place, get the icon, name and location.
-    const bounds = new google.maps.LatLngBounds();
-
-    places.forEach((place) => {
-      if (!place.geometry || !place.geometry.location) {
-        console.log("Returned place contains no geometry");
-        return;
-      }
-
-      const icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25),
-      };
-
-      // Create a marker for each place.
-      markers.push(
-        new google.maps.Marker({
-          map,
-          icon,
-          title: place.name,
-          position: place.geometry.location,
-        }),
-      );
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-    map.fitBounds(bounds);
-  });
-}
-
-window.initAutocomplete = initAutocomplete;
+  
