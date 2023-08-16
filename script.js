a35ea1a656ca4736e566aa177d583c99f4b3a5be
RahiminASC23 <ramohammed@stu.allstarcode.org>
2023-08-16 09:52:54 -0400

    better maybe

--- script.js
+++ script.js
@@ -17,36 +17,55 @@ function appearOnScroll() {
 }
 
 // API CODE
-document.getElementById("zipcode-form").addEventListener("submit", function (e) {
-    e.preventDefault();
-
-    const zipcode = document.getElementById("zipcode-input").value;
-    const apiKey = "fsq3y0lGHgGJwsHZEldogtami5ozh65BAM4qhB4bObZViIU="; 
-
-    fetch(`https://api.foursquare.com/v3/venues/search?near=${zipcode}&radius=80500&query=solar+panel+providers&client_id=${apiKey}&v=20230815`)
-      .then(response => response.json())
-      .then(data => {
-        const venues = data.response.venues;
-        displayVenues(venues);
-      })
-      .catch(error => console.error(error));
-  });
-
-  function displayVenues(venues) {
-    const resultsContainer = document.getElementById("results");
-    resultsContainer.innerHTML = ""; // Clear previous results
-
-    venues.forEach(venue => {
-      const name = venue.name;
-      const address = venue.location.address || "Address not available";
-      const phone = venue.contact.phone || "Phone number not available";
-
-      const venueElement = document.createElement("div");
-      venueElement.className = "venue";
-      venueElement.innerHTML = `<h3>${name}</h3><p>${address}</p><p>${phone}</p>`;
-
-      resultsContainer.appendChild(venueElement);
+async function placeSearch(zipcode) {
+  try {
+    const searchParams = new URLSearchParams({
+      query: 'solar panel providers',
+      near: zipcode,
+      radius: 80500, // 50 miles in meters
+      open_now: 'true',
+      sort: 'DISTANCE',
     });
+    const results = await fetch(
+      `https://api.foursquare.com/v3/places/search?${searchParams}`,
+      {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          Authorization: 'fsq3y0lGHgGJwsHZEldogtami5ozh65BAM4qhB4bObZViIU=',
+        },
+      }
+    );
+    const data = await results.json();
+    return data;
+  } catch (err) {
+    console.error(err);
   }
+}
 
-  
+document.getElementById('zipcode-form').addEventListener('submit', async function (e) {
+  e.preventDefault();
+
+  const zipcode = document.getElementById('zipcode-input').value;
+  const data = await placeSearch(zipcode);
+
+  const venues = data.response.venues;
+  displayVenues(venues);
+});
+
+function displayVenues(venues) {
+  const resultsContainer = document.getElementById('results');
+  resultsContainer.innerHTML = ''; 
+
+  venues.forEach((venue) => {
+    const name = venue.name;
+    const address = venue.location.address || 'Address not available';
+    const phone = venue.contact.phone || 'Phone number not available';
+
+    const venueElement = document.createElement('div');
+    venueElement.className = 'venue'; 
+    venueElement.innerHTML = `<h3>${name}</h3><p>${address}</p><p>${phone}</p>`;
+
+    resultsContainer.appendChild(venueElement);
+  });
+}
